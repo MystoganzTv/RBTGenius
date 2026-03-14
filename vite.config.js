e15776c3173aa83fdf9af2ec93b99b97a3f04169
Enrique Padron Spångberg <enrique.padron853@gmail.com>
2026-03-14 16:56:07 -0400

    Reduce Vite log verbosity

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "node:url";
 
 export default defineConfig({
+  logLevel: "error",
   plugins: [react()],
   resolve: {
     alias: {
