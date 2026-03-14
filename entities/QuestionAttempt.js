8bc65b99a1d02573a4f9c0706cada66a8aa6ea70
Enrique Padron Spångberg <enrique.padron853@gmail.com>
2026-03-14 15:49:08 -0400

    Update question attempt schema

--- entities/QuestionAttempt.js
+++ entities/QuestionAttempt.js
@@ -1,9 +1,9 @@
 export default class QuestionAttempt {
   constructor(data = {}) {
-    this.id = data.id ?? "";
-    this.questionId = data.questionId ?? "";
-    this.selectedAnswer = data.selectedAnswer ?? null;
-    this.isCorrect = data.isCorrect ?? false;
-    this.answeredAt = data.answeredAt ?? null;
+    this.question_id = data.question_id ?? "";
+    this.selected_answer = data.selected_answer ?? "";
+    this.is_correct = data.is_correct ?? false;
+    this.topic = data.topic ?? "measurement";
+    this.time_spent_seconds = data.time_spent_seconds ?? 0;
   }
 }
