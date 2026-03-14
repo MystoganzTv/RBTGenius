07ae1f6d0cece0badc99f82bdf69b186f05dd1da
Enrique Padron Spångberg <enrique.padron853@gmail.com>
2026-03-14 16:15:54 -0400

    Refine skeleton UI component

--- src/components/ui/skeleton.jsx
+++ src/components/ui/skeleton.jsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 function Skeleton({ className, ...props }) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-slate-200/80", className)}
+      className={cn("animate-pulse rounded-md bg-brand-100", className)}
       {...props}
     />
   );
